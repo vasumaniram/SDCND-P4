82a973ca4353f5909cd8884dd1e262ff3b34096b
Manikandan <vasumaniram@gmail.com>
2018-12-27 16:58:34 +0000

    Generator Added

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ def get_training_data():
     correction_factor = [0.0,0.2,-0.2]
     for line in lines:
         for i,position in enumerate(['center','left','right']):
-            source_path = line['center']
+            source_path = line[position]
             file_name = source_path.split('/')[-1]
             image = cv2.imread(current_path + file_name)
             image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
@@ -47,7 +47,44 @@ def cnn_model(X_train,y_train):
     model.compile(loss='mse', optimizer='adam')
     model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=5)
     model.save('model.h5')
-    
+samples = []    
+with open('../data/driving_log.csv') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for line in reader:
+        samples.append(line)
+from sklearn.model_selection import train_test_split
+train_samples,validation_samples = train_test_split(samples,test_size=0.2)
+import sklearn
+def generator(samples,batch_size=64):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        current_path = '../data/IMG/'
+        correction_factor = [0.0,0.2,-0.2]
+        flipped_correct_factor = [0.0,-0.2,0.2]
+        for offset in range(0,num_samples,batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i,position in enumerate(['center','left','right']):
+                    source_path = line[position]
+                    file_name = source_path.split('/')[-1]
+                    image = cv2.imread(current_path + file_name)
+                    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+                    images.append(image)
+                    angle = float(line['steering'])
+                    angles.append(angle-correction_factor[i])
+                    image_flipped = np.fliplr(image)
+                    images.append(image_flipped)
+                    angles.append(-angle-flipped_correct_factor[i])
+                X_train = np.array(images)
+                y_train = np.array(angles)
+                yield sklearn.utils.shuffle(X_train,y_train)
+train_generator = generator(train_samples,batch_size=32)
+validation_generator = generator(validation_samples,batch_size=32)
+
+                
 def pipeline():    
     X_train,y_train = get_training_data()
     print(len(y_train))
